7862fcaab4218ac1c8c34c90ffe8ae62c5eed52b
Daniel Vanko <daaniiieel@disroot.org>
2020-10-25 15:53:05 +0100

    pdf debug screen

--- lib/modules/printing/printerDebugScreen.dart
+++ lib/modules/printing/printerDebugScreen.dart
@@ -7,9 +7,9 @@ import 'package:pdf/pdf.dart';
 class PrintingDebugScreen extends StatelessWidget {
   final Future<Uint8List> Function(PdfPageFormat) builder;
 
-  PrintingDebugScreen([
+  PrintingDebugScreen(
     this.builder,
-  ]);
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(child: PdfPreview(build: this.builder)));
