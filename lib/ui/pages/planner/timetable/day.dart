a8a97b039341bc9a59e313917c279932faf2082b
bru02 <bru02@freemail.hu>
2020-11-21 17:33:42 +0100

    feat: handle empty lessons

--- lib/ui/pages/planner/timetable/day.dart
+++ lib/ui/pages/planner/timetable/day.dart
@@ -1,12 +1,26 @@
+import 'dart:math';
 import 'package:filcnaplo/data/models/lesson.dart';
 import 'package:filcnaplo/ui/pages/planner/timetable/tile.dart';
-import 'package:flutter/material.dart';
 
 class Day {
   List<Lesson> lessons;
-  List<LessonTile> tiles;
   DateTime date;
-  Widget specialDate;
 
-  Day({this.lessons = const [], this.tiles = const [], this.date, this.specialDate});
+  Day({this.lessons = const [], this.date});
+
+  List<LessonTile> get tiles {
+    var lessonIndexes = lessons.map((l) => int.parse(l.lessonIndex));
+    int minIndex = lessonIndexes.reduce(min);
+    int maxIndex = lessonIndexes.reduce(max);
+
+    List<LessonTile> tiles = [];
+
+    new List<int>.generate(maxIndex - minIndex + 1, (int i) => minIndex + i)
+        .forEach((int i) {
+      var lesson = lessons.firstWhere((l) => int.parse(l.lessonIndex) == i,
+          orElse: () => Lesson.fromJson({'isEmpty': true, 'Oraszam': i}));
+      tiles.add(LessonTile(lesson));
+    });
+    return tiles;
+  }
 }
