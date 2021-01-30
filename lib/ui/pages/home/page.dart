fb63d117549833e5c258eaa5991dd48d999c6143
RedyAu <fodor.benedek2001@gmail.com>
2021-01-30 16:36:36 +0100

    no hello no hi

--- lib/ui/pages/home/page.dart
+++ lib/ui/pages/home/page.dart
@@ -29,7 +29,6 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     if (newData()) {
       _feedBuilder.build();
-      print("hello hi");
       saveKeyes();
     }
 
