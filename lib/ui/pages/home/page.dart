70ca6df5942e6d6df697989732346e6876376182
RedyAu <fodor.benedek2001@gmail.com>
2021-01-30 18:00:31 +0100

    fix empty page

--- lib/ui/pages/home/page.dart
+++ lib/ui/pages/home/page.dart
@@ -22,6 +22,7 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     _feedBuilder = FeedBuilder(callback: () => setState(() {}));
+    _feedBuilder.build();
     saveKeyes();
   }
 
