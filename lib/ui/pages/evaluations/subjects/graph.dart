0d9e4ecc65b6a9b41e177c642bd0e1ac686127f9
RedyAu <fodor.benedek2001@gmail.com>
2021-04-18 14:24:18 +0200

    subject graph - don't scroll page when dragging graph per https://github.com/imaNNeoFighT/fl_chart/issues/637#issuecomment-818068591

--- lib/ui/pages/evaluations/subjects/graph.dart
+++ lib/ui/pages/evaluations/subjects/graph.dart
@@ -54,7 +54,8 @@ class _SubjectGraphState extends State<SubjectGraph> {
       ));
     });
 
-    return Container(
+    return GestureDetector(
+      onHorizontalDragUpdate: (_) {},
       child: LineChart(
         LineChartData(
           lineBarsData: [
