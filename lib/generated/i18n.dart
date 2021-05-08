1a062fc69da01370b546a078120d2efed9ebc37d
mtlhdnrd <mtldhnrd@protonmail.com>
2021-05-08 14:59:21 +0200

    Pressing the week text in timetable goes to current week

--- lib/generated/i18n.dart
+++ lib/generated/i18n.dart
@@ -160,6 +160,8 @@ class I18n implements WidgetsLocalizations {
   String get dateWeekPrev => "előző hét";
   /// "következő hét"
   String get dateWeekNext => "következő hét";
+  /// "Mostani hét"
+  String get dateWeekCurrent => "Mostani hét";
   /// "jan"
   String get dateJan => "jan";
   /// "feb"
@@ -954,6 +956,9 @@ class _I18n_en_US extends I18n {
   /// "next week"
   @override
   String get dateWeekNext => "next week";
+  /// "Current week"
+  @override
+  String get dateWeekCurrent => "Current week";
   /// "jan"
   @override
   String get dateJan => "jan";
@@ -2039,6 +2044,9 @@ class _I18n_de_DE extends I18n {
   /// "nächste Woche"
   @override
   String get dateWeekNext => "nächste Woche";
+  /// "Jetzige Woche"
+  @override
+  String get dateWeekCurrent => "Jetzige Woche";
   /// "jan"
   @override
   String get dateJan => "jan";
